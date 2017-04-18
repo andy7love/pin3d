07b24175a3ca78b52e04e5326b00460104218855
Andres Amor <andy.7love@gmail.com>
2017-04-18 19:15:41 -0300

    Better cards styles + using masonry for layouting + more model examples.

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,29 +4,131 @@ require('styles/App.scss');
 
 import React from 'react';
 import ModelCard from './ModelCard.js';
+import Masonry from 'react-masonry-component';
 //import { Card } from 'semantic-ui-react';
 
-let sample1 = require('../images/sample-models/sample1.thumb.jpg');
+const masonryOptions = {
+    itemSelector: '.card',
+    gutter: 20,
+    initLayout: true,
+    transitionDuration: 0
+};
 
 const items = [
   {
     id: 1,
-    name: 'Project Report - April',
-    description: 'Leverage agile frameworks to provide a robust synopsis for high level overviews.',
-    uploaded: '10 minutes ago',
-    image: sample1
+    name: 'Urban Helicopter',
+    description: 'Lorem ipsum dolor sit amet.',
+    uploaded: '20m',
+    tags: ['transport', 'flying'],
+    likes: 17,
+    comments: ['cool!', 'nice!', 'great!'],
+    image: require('../images/sample-models/sample1.thumb.jpg'),
+    author: {
+      name: 'Elliot',
+      avatar: require('../images/avatars/elliot.jpg')
+    }
+  },
+  {
+    id: 2,
+    name: 'City Bridge',
+    description: 'Lorem ipsum dolor sit amet.',
+    uploaded: '32m',
+    tags: ['building', 'urban'],
+    likes: 4,
+    comments: ['cool!'],
+    image: require('../images/sample-models/sample2.thumb.jpg'),
+    author: {
+      name: 'Jenny',
+      avatar: require('../images/avatars/jenny.jpg')
+    }
+  },
+  {
+    id: 3,
+    name: 'Classic car',
+    description: 'Lorem ipsum dolor sit amet.',
+    uploaded: '45m',
+    tags: ['transport', 'car', 'classic'],
+    likes: 31,
+    comments: ['cool!', 'nice!', 'great!', 'love it!', 'impresive!', 'wonderfull!'],
+    image: require('../images/sample-models/sample3.thumb.jpg'),
+    author: {
+      name: 'Matthew',
+      avatar: require('../images/avatars/matthew.png')
+    }
+  },
+  {
+    id: 4,
+    name: 'Urban buildings',
+    description: 'Lorem ipsum dolor sit amet.',
+    uploaded: '7h',
+    tags: ['transport', 'flying'],
+    likes: 10,
+    comments: ['cool!'],
+    image: require('../images/sample-models/sample4.thumb.jpg'),
+    author: {
+      name: 'Elyse',
+      avatar: require('../images/avatars/elyse.png')
+    }
+  },
+  {
+    id: 5,
+    name: 'Car engine',
+    description: 'Lorem ipsum dolor sit amet.',
+    uploaded: '10h',
+    tags: ['industrial', 'engine'],
+    likes: 7,
+    comments: ['nice!', 'great!'],
+    image: require('../images/sample-models/sample5.thumb.jpg'),
+    author: {
+      name: 'Elliot',
+      avatar: require('../images/avatars/elliot.jpg')
+    }
+  },
+  {
+    id: 6,
+    name: 'City Hospital',
+    description: 'Lorem ipsum dolor sit amet.',
+    uploaded: '14h',
+    tags: ['building', 'urban'],
+    likes: 21,
+    comments: ['cool!', 'nice!', 'great!', 'love it!', 'impresive!'],
+    image: require('../images/sample-models/sample6.thumb.jpg'),
+    author: {
+      name: 'Jenny',
+      avatar: require('../images/avatars/jenny.jpg')
+    }
+  },
+  {
+    id: 7,
+    name: 'Cessna 172',
+    description: 'Lorem ipsum dolor sit amet.',
+    uploaded: '5d',
+    tags: ['transport', 'flying'],
+    likes: 21,
+    comments: ['great!', 'love it!', 'impresive!'],
+    image: require('../images/sample-models/sample7.thumb.jpg'),
+    author: {
+      name: 'Elyse',
+      avatar: require('../images/avatars/elyse.png')
+    }
   }
 ];
 
 class AppComponent extends React.Component {
   render() {
     let cards = items.map(item => {
-      return <ModelCard key={item.id} name={item.name} description={item.description} uploaded={item.uploaded} image={item.image} />;
+      return <ModelCard key={item.id} model={item} />;
     });
 
     return (
       <div className="index">
-        {cards}
+        <Masonry
+            options={masonryOptions}
+            disableImagesLoaded={false}
+            updateOnEachImageLoad={false}>
+            {cards}
+        </Masonry>
       </div>
     );
   }
--- src/components/ModelCard.js
+++ src/components/ModelCard.js
@@ -4,28 +4,30 @@ import { Card, Icon, Image } from 'semantic-ui-react'
 class ModelCard extends React.Component {
   render() {
     return <Card>
-      <Image src={this.props.image} />
       <Card.Content>
-        <Card.Header>{this.props.name}</Card.Header>
-        <Card.Meta>{this.props.uploaded}</Card.Meta>
-        <Card.Description>{this.props.description}</Card.Description>
+        <div className="right floated meta">{this.props.model.uploaded}</div>
+        <Card.Header className="left floated">
+          {this.props.model.name}
+        </Card.Header>
+      </Card.Content>
+      <div className="image">
+          <Image src={this.props.model.image} />
+      </div>
+      <Card.Content>
+        <Icon name="comment" />
+        {this.props.model.comments.length} comments
+        <span className="right floated">
+          <Icon name="heart" className="outline" />
+          {this.props.model.likes} likes
+        </span>
       </Card.Content>
       <Card.Content extra>
-        <a>
-          <Icon name='tag' /> 10
-        </a>
-
-        <a>
-          <Icon name='comments' /> 10
-        </a>
-
-        <a>
-          <Icon name='share alternate' /> 10
-        </a>
-
-        <a>
-          <Icon name='star' /> 10
-        </a>
+        <div className="left floated">
+          <Icon name="tags" /> {this.props.model.tags.join(', ')}
+        </div>
+        <div className="right floated author">
+          <Image size='mini' avatar src={this.props.model.author.avatar} /> {this.props.model.author.name}
+        </div>
       </Card.Content>
     </Card>;
   }
